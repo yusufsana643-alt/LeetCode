1181ac787086e86ad84f0d4a531825e970a48b72
yusufsana643-alt <yusufsana643@gmail.com>
2026-08-25 00:00:54 +0530

    Time: 3 ms (100%), Space: 92.3 MB (29.36%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int stoneGameVIII(int[] stones) {
+
+        int n = stones.length;
+
+        int[] prefix = new int[n];
+
+        prefix[0] = stones[0];
+
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + stones[i];
+        }
+
+        int dp = prefix[n - 1];
+
+        for (int i = n - 2; i >= 1; i--) {
+            dp = Math.max(dp, prefix[i] - dp);
+        }
+
+        return dp;
+    }
+}
